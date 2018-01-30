b856d193175caededdbbb57514f4efd9e5ef3176
gelito <angel.quesada@kubide.es>
2018-01-30 19:49:21 +0100

    Our Smart Contract and the basic tests

--- contracts/OurToken.sol
+++ contracts/OurToken.sol
@@ -1,17 +1,18 @@
 pragma solidity ^0.4.17;
 
-
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "zeppelin-solidity/contracts/token/ERC827/ERC827Token.sol";
 
 
-contract OurToken is MintableToken {
-    string public name = "OurToken";
-    string public symbol = "OT";
-    uint8 public decimals = 10;
-    uint public initialSupply = 1000000;
+contract OurToken is ERC827Token, MintableToken {
+  string public name = "OurToken";
+  string public symbol = "OT";
+  uint8 public decimals = 10;
+  uint public initialSupply = 2000001;
 
-    function OurToken() public {
-        balances[msg.sender] = initialSupply;
-    }
+  function OurToken() public {
+    balances[msg.sender] = initialSupply;
+    totalSupply_ = initialSupply;
+  }
 
 }
